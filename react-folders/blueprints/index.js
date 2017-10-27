758860b41bba15df337e0fcda2c28a4b66d4b8b4
Leigh Barnes <leighbrendonbarnes@gmail.com>
2017-10-27 13:47:35 +0100

    Initial

---
+++ react-folders/blueprints/index.js
@@ -0,0 +1,1 @@
+// potentially Export some mock API bluepriunt data from here.
---
+++ react-folders/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Routes extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (<div></div>);
+  }
+
+  componentDidMount() {
+  }
+}
+
+export default Routes;
